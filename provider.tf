961d65c1804b35934389ec91c65afa2317d426ca
rsanofi <rsanofi0625@outlook.com>
2025-06-17 22:13:00 +0530

    update

--- provider.tf
+++ provider.tf
@@ -1,10 +1,14 @@
-provider "azurerm" {
-  version = "~> 4.20.0"
-  features {}
-}
-
-# Set the terraform backend
 terraform {
   required_version = ">= 1.0"
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.30.0"
+    }
+  }
   backend "azurerm" {}
+}
+
+provider "azurerm" {
+  features {}
 }
